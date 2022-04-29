22eddbd9de07b4b06231fc5ff3210176027d2e9e
Adfer Aijaz <46266480+adferaijaz@users.noreply.github.com>
2022-04-30 05:14:43 +0530

    OK
    Digital Popup ID

---
+++ script.js
@@ -0,0 +1,15 @@
+(function () {
+    const heart = document.getElementById("heart");
+    heart.addEventListener("click", function () {
+      heart.classList.toggle("red");
+    });
+  })();
+
+
+  (function () {
+    const heart1 = document.getElementById("heart1");
+    heart1.addEventListener("click", function () {
+      heart1.classList.toggle("red");
+    });
+  })();
+  
